3a5b966dd7aa6bff481ca495f44f256d22e71a03
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-02-24 11:17:38 +0800

    Use LVGL.Windows instead of LVGL.Core and LVGL.Examples.

--- LVGL.Windows/lv_conf.h
+++ LVGL.Windows/lv_conf.h
@@ -218,7 +218,7 @@ typedef void * lv_fs_drv_user_data_t;
 #define LV_USE_USER_DATA        1
 
 /*1: Show CPU usage and FPS count in the right bottom corner*/
-#define LV_USE_PERF_MONITOR     0
+#define LV_USE_PERF_MONITOR     1
 
 /*1: Use the functions and types from the older API if possible */
 #define LV_USE_API_EXTENSION_V6  1
