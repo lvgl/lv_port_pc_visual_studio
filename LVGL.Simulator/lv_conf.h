e0ec91ad7fe6e3b7b7cb92ec09eb42f872620486
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-06-17 11:09:32 +0800

    Update LVGL configuration file.

--- LVGL.Simulator/lv_conf.h
+++ LVGL.Simulator/lv_conf.h
@@ -1,6 +1,6 @@
 ﻿/**
  * @file lv_conf.h
- * Configuration file for v8.0.0-dev
+ * Configuration file for v8.1.0-dev
  */
 
 /*
@@ -27,7 +27,7 @@
 #define LV_COLOR_16_SWAP   0
 
 /*Enable more complex drawing routines to manage screens transparency.
- *Can be used if the UI is above an other layer, e.g. an OSD menu or video player.
+ *Can be used if the UI is above another layer, e.g. an OSD menu or video player.
  *Requires `LV_COLOR_DEPTH = 32` colors and the screen's `bg_opa` should be set to non LV_OPA_COVER value*/
 #define LV_COLOR_SCREEN_TRANSP    0
 
@@ -284,7 +284,7 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 #define LV_FONT_MONTSERRAT_46    1
 #define LV_FONT_MONTSERRAT_48    1
 
-/*Demonstrate special features*/
+ /*Demonstrate special features*/
 #define LV_FONT_MONTSERRAT_12_SUBPX      1
 #define LV_FONT_MONTSERRAT_28_COMPRESSED 1  /*bpp = 3*/
 #define LV_FONT_DEJAVU_16_PERSIAN_HEBREW 1  /*Hebrew, Arabic, Perisan letters and all their forms*/
@@ -353,9 +353,9 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 #define LV_USE_BIDI         1
 #if LV_USE_BIDI
 /*Set the default direction. Supported values:
- *`LV_BIDI_DIR_LTR` Left-to-Right
- *`LV_BIDI_DIR_RTL` Right-to-Left
- *`LV_BIDI_DIR_AUTO` detect texts base direction*/
+ *`LV_BASE_DIR_LTR` Left-to-Right
+ *`LV_BASE_DIR_RTL` Right-to-Left
+ *`LV_BASE_DIR_AUTO` detect texts base direction*/
 #define LV_BIDI_BASE_DIR_DEF  LV_BASE_DIR_AUTO
 #endif
 
@@ -371,6 +371,8 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 
 #define LV_USE_ARC          1
 
+#define LV_USE_ANIMIMG	    1
+
 #define LV_USE_BAR          1
 
 #define LV_USE_BTN          1
@@ -381,7 +383,6 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 
 #define LV_USE_CHECKBOX     1
 
-#define LV_USE_CHART        1
 
 #define LV_USE_DROPDOWN     1   /*Requires: lv_label*/
 
@@ -389,13 +390,11 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 
 #define LV_USE_LABEL        1
 #if LV_USE_LABEL
-#  define LV_LABEL_TEXT_SEL         1   /*Enable selecting text of the label*/
+#  define LV_LABEL_TEXT_SELECTION         1   /*Enable selecting text of the label*/
 #  define LV_LABEL_LONG_TXT_HINT    1   /*Store some extra info in labels to speed up drawing of very long texts*/
 #endif
 
 #define LV_USE_LINE         1
-
-#define LV_USE_METER        1
 
 #define LV_USE_ROLLER       1   /*Requires: lv_label*/
 #if LV_USE_ROLLER
@@ -434,6 +433,8 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 # define LV_USE_CALENDAR_HEADER_DROPDOWN    1
 #endif  /*LV_USE_CALENDAR*/
 
+#define LV_USE_CHART        1
+
 #define LV_USE_COLORWHEEL   1
 
 #define LV_USE_IMGBTN       1
@@ -443,6 +444,8 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 #define LV_USE_LED          1
 
 #define LV_USE_LIST         1
+
+#define LV_USE_METER        1
 
 #define LV_USE_MSGBOX       1
 
@@ -456,6 +459,12 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 
 #define LV_USE_WIN          1
 
+#define LV_USE_SPAN         1
+#if LV_USE_SPAN
+/*A line text can contain maximum num of span descriptor */
+#  define LV_SPAN_SNIPPET_STACK_SIZE   64
+#endif
+
 /*-----------
  * Themes
  *----------*/
@@ -463,8 +472,8 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 #define LV_USE_THEME_DEFAULT    1
 #if LV_USE_THEME_DEFAULT
 
-/*1: Light mode; 0: Dark mode*/
-# define LV_THEME_DEFAULT_PALETTE_LIGHT     1
+/*0: Light mode; 1: Dark mode*/
+# define LV_THEME_DEFAULT_DARK     0
 
 /*1: Enable grow on press*/
 # define LV_THEME_DEFAULT_GROW              1
@@ -475,6 +484,9 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 
 /*An very simple them that is a good starting point for a custom theme*/
  #define LV_USE_THEME_BASIC    1
+
+/*A theme designed for monochrome displays*/
+#define LV_USE_THEME_MONO       1
 
 /*-----------
  * Layouts
