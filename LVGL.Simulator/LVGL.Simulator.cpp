14eb32129071623b1984e85f8d3d4e6529162731
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-07-18 19:33:36 +0800

    Implement lv_win32_filesystem_driver_error_from_win32.

--- LVGL.Simulator/LVGL.Simulator.cpp
+++ LVGL.Simulator/LVGL.Simulator.cpp
@@ -32,6 +32,132 @@
 #pragma warning(pop)
 #endif
 
+lv_fs_res_t lv_win32_filesystem_driver_error_from_win32(
+    DWORD Error)
+{
+    lv_fs_res_t res;
+
+    switch (Error)
+    {
+    case ERROR_SUCCESS:
+        res = LV_FS_RES_OK;
+        break;
+    case ERROR_BAD_UNIT:
+    case ERROR_NOT_READY:
+    case ERROR_CRC:
+    case ERROR_SEEK:
+    case ERROR_NOT_DOS_DISK:
+    case ERROR_WRITE_FAULT:
+    case ERROR_READ_FAULT:
+    case ERROR_GEN_FAILURE:
+    case ERROR_WRONG_DISK:
+        res = LV_FS_RES_HW_ERR;
+        break;
+    case ERROR_INVALID_HANDLE:
+    case ERROR_INVALID_TARGET_HANDLE:
+        res = LV_FS_RES_FS_ERR;
+        break;
+    case ERROR_FILE_NOT_FOUND:
+    case ERROR_PATH_NOT_FOUND:
+    case ERROR_INVALID_DRIVE:
+    case ERROR_NO_MORE_FILES:
+    case ERROR_SECTOR_NOT_FOUND:
+    case ERROR_BAD_NETPATH:
+    case ERROR_BAD_NET_NAME:
+    case ERROR_BAD_PATHNAME:
+    case ERROR_FILENAME_EXCED_RANGE:
+        res = LV_FS_RES_NOT_EX;
+        break;
+    case ERROR_DISK_FULL:
+        res = LV_FS_RES_FULL;
+        break;
+    case ERROR_SHARING_VIOLATION:
+    case ERROR_LOCK_VIOLATION:
+    case ERROR_DRIVE_LOCKED:
+        res = LV_FS_RES_LOCKED;
+        break;
+    case ERROR_ACCESS_DENIED:
+    case ERROR_CURRENT_DIRECTORY:
+    case ERROR_WRITE_PROTECT:
+    case ERROR_NETWORK_ACCESS_DENIED:
+    case ERROR_CANNOT_MAKE:
+    case ERROR_FAIL_I24:
+    case ERROR_SEEK_ON_DEVICE:
+    case ERROR_NOT_LOCKED:
+    case ERROR_LOCK_FAILED:
+        res = LV_FS_RES_DENIED;
+        break;
+    case ERROR_BUSY:
+        res = LV_FS_RES_BUSY;
+        break;
+    case ERROR_TIMEOUT:
+        res = LV_FS_RES_TOUT;
+        break;
+    case ERROR_NOT_SAME_DEVICE:
+    case ERROR_DIRECT_ACCESS_HANDLE:
+        res = LV_FS_RES_NOT_IMP;
+        break;
+    case ERROR_TOO_MANY_OPEN_FILES:
+    case ERROR_ARENA_TRASHED:
+    case ERROR_NOT_ENOUGH_MEMORY:
+    case ERROR_INVALID_BLOCK:
+    case ERROR_OUT_OF_PAPER:
+    case ERROR_SHARING_BUFFER_EXCEEDED:
+    case ERROR_NOT_ENOUGH_QUOTA:
+        res = LV_FS_RES_OUT_OF_MEM;
+        break;
+    case ERROR_INVALID_FUNCTION:
+    case ERROR_INVALID_ACCESS:
+    case ERROR_INVALID_DATA:
+    case ERROR_BAD_COMMAND:
+    case ERROR_BAD_LENGTH:
+    case ERROR_INVALID_PARAMETER:
+    case ERROR_NEGATIVE_SEEK:
+        res = LV_FS_RES_INV_PARAM;
+        break;
+    default:
+        res = LV_FS_RES_UNKNOWN;
+        break;
+    }
+
+    return res;
+}
+
+//void* lv_win32_filesystem_driver_open_callback(
+//    lv_fs_drv_t* drv,
+//    const char* path,
+//    lv_fs_mode_t mode)
+//{
+//    UNREFERENCED_PARAMETER(drv);
+//
+//
+//
+//}
+//
+//void fuck()
+//{
+//    static lv_fs_drv_t filesystem_driver;
+//    lv_fs_drv_init(&filesystem_driver);
+//
+//    filesystem_driver.letter;
+//    filesystem_driver.ready_cb;
+//
+//    filesystem_driver.open_cb;
+//    filesystem_driver.close_cb;
+//    filesystem_driver.read_cb;
+//    filesystem_driver.write_cb;
+//    filesystem_driver.seek_cb;
+//    filesystem_driver.tell_cb;
+//
+//    filesystem_driver.dir_open_cb;
+//    filesystem_driver.dir_read_cb;
+//    filesystem_driver.dir_close_cb;
+//
+//    filesystem_driver.user_data;
+//
+//    lv_fs_drv_register(&filesystem_driver);
+//}
+
 int main()
 {
     lv_init();
