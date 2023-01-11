749e31e03c5b7044d77eec37860246e46b8c6205
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-01-12 00:43:20 +0800

    Add GetGitRepositoryRoot method for helping write maintainer specific tools.

--- LvglMaintainerTools/LvglSubmoduleProjectFileGenerator/Program.cs
+++ LvglMaintainerTools/LvglSubmoduleProjectFileGenerator/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace LvglSubmoduleProjectFileGenerator
+﻿using System.Diagnostics;
+
+namespace LvglSubmoduleProjectFileGenerator
 {
     internal class Program
     {
+        private static string GetGitRepositoryRoot()
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    FileName = "git.exe",
+                    Arguments = "rev-parse --show-toplevel"
+                }
+            };
+
+            if (process.Start())
+            {
+                process.WaitForExit();
+                if (process.ExitCode == 0)
+                {
+                    string? result = process.StandardOutput.ReadLine();
+                    if (result != null)
+                    {
+                        return Path.GetFullPath(result);
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+
         static void Main(string[] args)
         {
+            string Root = GetGitRepositoryRoot();
+
+            Console.WriteLine(Root);
+
             Console.WriteLine("Hello, World!");
+
+            Console.ReadKey();
         }
     }
 }
