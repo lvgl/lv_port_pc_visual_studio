57c2266daa4c296a6f6728d4993d32f2a244591b
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-01-27 14:36:24 +0800

    Implement GitRepositoryUtilities to LvglSubmoduleProjectFileGenerator.

--- LvglMaintainerTools/LvglSubmoduleProjectFileGenerator/Program.cs
+++ LvglMaintainerTools/LvglSubmoduleProjectFileGenerator/Program.cs
@@ -1,42 +1,11 @@
-﻿using System.Diagnostics;
-
-namespace LvglSubmoduleProjectFileGenerator
+﻿namespace LvglSubmoduleProjectFileGenerator
 {
+
     internal class Program
     {
-        private static string GetGitRepositoryRoot()
-        {
-            Process process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    CreateNoWindow = true,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    FileName = "git.exe",
-                    Arguments = "rev-parse --show-toplevel"
-                }
-            };
-
-            if (process.Start())
-            {
-                process.WaitForExit();
-                if (process.ExitCode == 0)
-                {
-                    string? result = process.StandardOutput.ReadLine();
-                    if (result != null)
-                    {
-                        return Path.GetFullPath(result);
-                    }
-                }
-            }
-
-            return string.Empty;
-        }
-
         static void Main(string[] args)
         {
-            string Root = GetGitRepositoryRoot();
+            string Root = GitRepositoryUtilities.GetRepositoryRoot();
 
             Console.WriteLine(Root);
 
