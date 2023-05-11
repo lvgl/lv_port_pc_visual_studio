3d8ced2e905e5f90d91fea7675b17956bd47a2eb
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-05-11 15:29:09 +0800

    Fix demo code when using lv_demo_benchmark.

--- LVGL.Simulator/LVGL.Simulator.cpp
+++ LVGL.Simulator/LVGL.Simulator.cpp
@@ -58,8 +58,8 @@ int main()
         return -1;
     }
 
-    lv_demo_widgets();
-    //lv_demo_benchmark();
+    //lv_demo_widgets();
+    lv_demo_benchmark(LV_DEMO_BENCHMARK_MODE_RENDER_AND_DRIVER);
 
     while (!lv_win32_quit_signal)
     {
