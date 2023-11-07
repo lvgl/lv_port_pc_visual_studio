0d38dd609d9889076f885ba81d850670c766aa1b
MouriNaruto <KurikoMouri@outlook.jp>
2023-11-07 20:38:11 +0800

    Adapt for the updated LVGL submodules.

--- LvglWindowsDesktopApplication/lv_conf.h
+++ LvglWindowsDesktopApplication/lv_conf.h
@@ -828,7 +828,7 @@
 #define LV_USE_DEMO_STRESS 1
 
 /*Music player demo*/
-#define LV_USE_DEMO_MUSIC 0
+#define LV_USE_DEMO_MUSIC 1
 #if LV_USE_DEMO_MUSIC
     #define LV_DEMO_MUSIC_SQUARE    1
     #define LV_DEMO_MUSIC_LANDSCAPE 1
