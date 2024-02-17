9d69e371d10c46e3f622430ef1b9e7bc275aafe2
MouriNaruto <Kenji.Mouri@outlook.com>
2024-02-17 16:40:34 +0800

    Start to add LVGL Windows Library project.

---
+++ LvglWindows/LvglWindows.cpp
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   Mile.Samples.DynamicLibrary
+ * FILE:      Mile.Samples.DynamicLibrary.cpp
+ * PURPOSE:   Implementation for Mile.Samples.DynamicLibrary
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Windows.h>
+
+EXTERN_C int WINAPI MileSamplesTestFunction()
+{
+    return 0;
+}
---
+++ LvglWindows/LvglWindowsStatic.cpp
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   Mile.Samples.StaticLibrary
+ * FILE:      Mile.Samples.StaticLibrary.cpp
+ * PURPOSE:   Implementation for Mile.Samples.StaticLibrary
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Windows.h>
+
+EXTERN_C int WINAPI MileSamplesTestFunction()
+{
+    return 0;
+}
