d6a4c6e51e656b1512fefaad621426a4368ed032
MouriNaruto <Kenji.Mouri@outlook.com>
2025-03-26 16:44:11 +0800

    Implement Lvgl.Build.Tasks.StaticLibraryExportsDefinitionGenerator task.

---
+++ Lvgl.Build.Tasks/StaticLibraryExportsDefinitionGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Mile.Project.Helpers;
+using System.Collections.Generic;
+
+namespace Lvgl.Build.Tasks
+{
+    public class StaticLibraryExportsDefinitionGenerator : Task
+    {
+        [Required]
+        public string InputLibraryFilePath { get; set; }
+
+        [Required]
+        public string TargetPlatform { get; set; }
+
+        [Required]
+        public string OutputExportsDefinitionPath { get; set; }
+
+        public override bool Execute()
+        {
+            ImageArchive.Archive Archive =
+                ImageArchive.Parse(InputLibraryFilePath);
+
+            if (Archive.Symbols != null)
+            {
+                SortedSet<string> Symbols = new SortedSet<string>();
+                foreach (string Symbol in ImageArchive.ListSymbols(
+                        Archive.Symbols,
+                        TargetPlatform == "x86"))
+                {
+                    if (Symbol.StartsWith("lv_") ||
+                        Symbol.StartsWith("_lv_"))
+                    {
+                        Symbols.Add(Symbol);
+                    }
+                }
+
+                Utilities.GenerateDefinitionFile(
+                    OutputExportsDefinitionPath,
+                    string.Empty,
+                    Symbols);
+            }
+
+            return true;
+        }
+    }
+}
