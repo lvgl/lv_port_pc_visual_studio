55617a33d0ef42becdd9d66d649fe1b12a6acb11
MouriNaruto <Kenji.Mouri@outlook.com>
2025-03-26 16:43:49 +0800

    Implement Lvgl.Build.Tasks.Utilities.GenerateDefinitionFile method.

---
+++ Lvgl.Build.Tasks/Utilities.cs
@@ -0,0 +1,25 @@
+﻿using Mile.Project.Helpers;
+using System.Collections.Generic;
+
+namespace Lvgl.Build.Tasks
+{
+    public class Utilities
+    {
+        public static void GenerateDefinitionFile(
+            string OutputFilePath,
+            string LibraryName,
+            SortedSet<string> Symbols)
+        {
+            string Content = string.Format(
+                "LIBRARY {0}\r\n\r\nEXPORTS\r\n\r\n",
+                LibraryName);
+
+            foreach (string Symbol in Symbols)
+            {
+                Content += string.Format("{0}\r\n", Symbol);
+            }
+
+            FileUtilities.SaveTextToFileAsUtf8Bom(OutputFilePath, Content);
+        }
+    }
+}
