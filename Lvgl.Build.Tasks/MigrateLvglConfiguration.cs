688535942a2de11ce13e5fdf86f58b420acc8d29
MouriNaruto <Kenji.Mouri@outlook.com>
2025-07-06 13:36:21 +0800

    Start to implement Lvgl.Build.Tasks.MigrateLvglConfiguration task.

---
+++ Lvgl.Build.Tasks/MigrateLvglConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Lvgl.Build.Tasks
+{
+    public class MigrateLvglConfiguration : Task
+    {
+        [Required]
+        public string TargetFilePath { get; set; }
+
+        [Required]
+        public string TemplateFilePath { get; set; }
+
+        public override bool Execute()
+        {
+            return true;
+        }
+    }
+}
