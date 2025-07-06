b9648adc52cb29eec0f2dfe7495103bd73a90374
MouriNaruto <Kenji.Mouri@outlook.com>
2025-07-06 13:42:53 +0800

    Continue to implement Lvgl.Build.Tasks.MigrateLvglConfiguration task.

--- Lvgl.Build.Tasks/MigrateLvglConfiguration.cs
+++ Lvgl.Build.Tasks/MigrateLvglConfiguration.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Lvgl.Build.Tasks
 {
@@ -10,6 +14,48 @@ namespace Lvgl.Build.Tasks
 
         [Required]
         public string TemplateFilePath { get; set; }
+
+        private static readonly Regex OptionRule = new Regex(
+            @"^#define\s+([A-Z0-9_]+)\s+((?:.|\n)*?)(?:\s*(?:\/\/|\/\*)|(?:\n\s*#))",
+            RegexOptions.Compiled | RegexOptions.Multiline);
+
+        private static SortedDictionary<string, string> ParseLvglConfiguration(
+            string FilePath)
+        {
+            SortedDictionary<string, string> Result =
+                new SortedDictionary<string, string>();
+
+            string Content = File.ReadAllText(FilePath, Encoding.UTF8);
+
+            // Normalize line endings to Unix style for easier processing.
+            Content = Content.Replace("\r\n", "\n").Replace('\r', '\n');
+
+            // Add a sentinel to the end of the file to ensure the last macro
+            // is matched correctly.
+            Content += "\n#";
+
+            MatchCollection Matches = OptionRule.Matches(Content);
+
+            foreach (Match MatchedResult in Matches)
+            {
+                if (!MatchedResult.Success)
+                {
+                    continue;
+                }
+
+                // Remove newlines and backslashes, merging multi-line values
+                // into one line.
+                string PreprocessedValue = Regex.Replace(
+                    MatchedResult.Groups[2].Value,
+                    @"\s*\\\s*\n\s*", " ").Trim();
+
+                Result.Add(
+                    MatchedResult.Groups[1].Value,
+                    PreprocessedValue);
+            }
+
+            return Result;
+        }
 
         public override bool Execute()
         {
