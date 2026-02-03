2e872a0f95daf10c42b20e31896b7a37d35da381
MouriNaruto <Kenji.Mouri@outlook.com>
2026-02-03 19:34:34 +0800

    Add Utilities.CheckProjectItemElementExists to LvglProjectFileUpdater.

---
+++ LvglProjectFileUpdater/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Build.Construction;
+
+namespace LvglProjectFileUpdater
+{
+    public class Utilities
+    {
+        public static bool CheckProjectItemElementExists(
+            ProjectRootElement RootElement,
+            string ItemType,
+            string Include)
+        {
+            foreach (ProjectItemElement Item in RootElement.Items)
+            {
+                if (ItemType == Item.ItemType && Include == Item.Include)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
