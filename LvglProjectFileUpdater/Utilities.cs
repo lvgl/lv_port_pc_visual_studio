2173405567a5a44fbdf68a5d2aea71ffef51201c
MouriNaruto <Kenji.Mouri@outlook.com>
2026-02-03 19:50:27 +0800

    Add Utilities.IsHeaderFile and Utilities.IsSourceFile to LvglProjectFileUpdater.

--- LvglProjectFileUpdater/Utilities.cs
+++ LvglProjectFileUpdater/Utilities.cs
@@ -4,6 +4,42 @@ namespace LvglProjectFileUpdater
 {
     public class Utilities
     {
+        public static bool IsHeaderFile(string FilePath)
+        {
+            switch (Path.GetExtension(FilePath).ToLowerInvariant())
+            {
+                case ".h":
+                case ".hh":
+                case ".hpp":
+                case ".hxx":
+                case ".h++":
+                case ".hm":
+                case ".inl":
+                case ".inc":
+                case ".ipp":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public static bool IsSourceFile(string FilePath)
+        {
+            switch (Path.GetExtension(FilePath).ToLowerInvariant())
+            {
+                case ".cpp":
+                case ".c":
+                case ".cc":
+                case ".cxx":
+                case ".c++":
+                case ".cppm":
+                case ".ixx":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
         public static bool CheckProjectItemElementExists(
             ProjectRootElement RootElement,
             string ItemType,
